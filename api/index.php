9366816cb18a91c08815f6257bfeb2f6f155d49f
kientp1610 <kientp1610@gmail.com>
2022-12-26 23:32:00 +0700

    test routes

--- api/index.php
+++ api/index.php
@@ -1,34 +1,53 @@
 <?php
+$routes = [];
+route("/", function () {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    $content = array();
+    $content["body"] = array();
+    for ($i = 1; $i <= 12; $i++) {
+        if ($i === 1) {
+            $url = "https://chiasemoi.com/sach-tom-tat";
+        } else {
+            $url = "https://chiasemoi.com/sach-tom-tat/page/" . $i;
+        }
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+        $api = file_get_contents($url);
+        preg_match_all('#<article class="item-list".*?>(.+?)</article>#si', $api, $match);
 
-$content = array();
-$content["body"] = array();
-
-for ($i = 1; $i <= 12; $i++) {
-    if ($i === 1) {
-        $url = "https://chiasemoi.com/sach-tom-tat";
-    } else {
-        $url = "https://chiasemoi.com/sach-tom-tat/page/" . $i;
-    }
-
-    $api = file_get_contents($url);
-    preg_match_all('#<article class="item-list".*?>(.+?)</article>#si', $api, $match);
-
-    if (!empty($match[1])) {
-        foreach ($match[1] as $item) {
-            preg_match('#<h2 class="post-box-title">(.+?)</h2>#si', $item, $a);
-            preg_match('#<a.*?>(.+?)</a>#si', $a[1], $title);
-            preg_match('#<a href="https://chiasemoi.com/(.*?).html">(.+?)</a>#si', $a[1], $href);
-            preg_match('#<img.*? src="(.*?)" class="attachment-tie-medium size-tie-medium wp-post-image" alt="" />#si', $item, $img);
-            $itemArr = array(
-                "img" => $img[1],
-                "href" => $href[1],
-                "title" => $title[1]
-            );
-            array_push($content["body"], $itemArr);
+        if (!empty($match[1])) {
+            foreach ($match[1] as $item) {
+                preg_match('#<h2 class="post-box-title">(.+?)</h2>#si', $item, $a);
+                preg_match('#<a.*?>(.+?)</a>#si', $a[1], $title);
+                preg_match('#<a href="https://chiasemoi.com/(.*?).html">(.+?)</a>#si', $a[1], $href);
+                preg_match('#<img.*? src="(.*?)" class="attachment-tie-medium size-tie-medium wp-post-image" alt="" />#si', $item, $img);
+                $itemArr = array(
+                    "img" => $img[1],
+                    "href" => $href[1],
+                    "title" => $title[1]
+                );
+                array_push($content["body"], $itemArr);
+            }
         }
     }
+    echo json_encode($content["body"]);
+});
+
+route("/login", function () {
+    echo "Login page";
+});
+function route($path, $callback)
+{
+    global $routes;
+    $routes[$path] = $callback;
 }
-echo json_encode($content["body"]);
+function run()
+{
+    global $routes;
+    $uri = $_SERVER["REQUEST_URI"];
+    foreach ($routes as $path => $callback) {
+        if ($path !== $uri) continue;
+        $callback();
+    }
+}
+run();
